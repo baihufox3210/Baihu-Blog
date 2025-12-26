035da380b9d89013b2423d3daa48bfaa89265921
baihu <baihu3210@gmail.com>
2025-12-26 19:27:07 +0800

    feat: initialize React project with Vite, including basic components, styling, and configuration

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Components/Header";
+import "./App.css"
+
+function App() {
+  return (
+    <div className="index">
+      <div className="container">
+        <Header />
+      </div>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default App
